940557e9477145a2db702a189aa0a0d824021389
yubintw <yubin.cs05g@nctu.edu.tw>
2022-04-01 02:02:03 +0800

    Add demo test case

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -25,4 +25,55 @@ describe('Todo app test', () => {
 
   // TODO: add other frontend test
 
+  it('should show default todo in todoList', async () => {
+    render(<App />)
+    const item0 = await screen.findByText('item0')
+    expect(item0).toBeInTheDocument()
+  })
+
+  test('Add button is disabled on default', async () => {
+    render(<App />)
+    const addButton = await screen.findByText('Add Todo')
+    expect(addButton).toBeDisabled()
+  })
+
+  test('Add button is enabled when name and description are not empty', async () => {
+    render(<App />)
+
+    const nameInput = await screen.findByTestId('name')
+    const descriptionInput = await screen.findByTestId('description')
+    await userEvent.type(nameInput, 'Prepare slide')
+    await userEvent.type(descriptionInput, 'slider for javascript testing workshop')
+
+    const addButton = await screen.findByText('Add Todo')
+    expect(addButton).toBeEnabled()
+  })
+
+  test('new todo will appearance, name and description input will be clear when click Add Todo button', async () => {
+    render(<App />)
+
+    const nameInput = await screen.findByTestId('name')
+    const descriptionInput = await screen.findByTestId('description')
+    await userEvent.type(nameInput, 'Prepare slide')
+    await userEvent.type(descriptionInput, 'slider for javascript testing workshop')
+    const addButton = await screen.findByText('Add Todo')
+    await userEvent.click(addButton)
+    const newTodo = await screen.findByText('Prepare slide')
+
+    expect(nameInput).toHaveTextContent('')
+    expect(descriptionInput).toHaveTextContent('')
+    expect(newTodo).toBeInTheDocument()
+  })
+
+  it('should be disappear when user click delete button of a todo', async () => {
+    render(<App />)
+
+    const deleteButtons = await screen.findAllByText('Delete')
+    await userEvent.click(deleteButtons[0])
+
+    await waitFor(() => {
+      const item0 = screen.queryByText('item0')
+      expect(item0).toBeNull()
+    })
+  })
 })
